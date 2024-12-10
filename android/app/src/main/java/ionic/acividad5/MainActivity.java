5744808281220f2c1425a75f847ecacc0b07e7be
RAINER-ORTIZ <rainer.ortiz@galileo.edu>
2024-12-09 22:46:11 -0600

    se sube el proyecto

---
+++ android/app/src/main/java/ionic/acividad5/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.acividad5;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.acividad5',
+  appName: 'acividad5',
+  webDir: 'www'
+};
+
+export default config;
